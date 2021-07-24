fc02cd215e82d53e7c7a65ef10c1bfead49e526c
GiovaniBecker78 <87495884+GiovaniBecker78@users.noreply.github.com>
2021-07-24 13:06:25 -0300

    criado as classes Report, ReportFactory, ReportType.

---
+++ src/main/java/Framework/Report.java
@@ -0,0 +1,41 @@
+package Framework;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+
+public class Report {
+
+    private static final ExtentReports extent = ReportFactory.getInstance();
+    private static final ThreadLocal<ExtentTest> parentTest = new ThreadLocal<>();
+    private static final ThreadLocal<ExtentTest> test = new ThreadLocal<>();
+
+    public static void createTest(String testName, ReportType type){
+        ExtentTest extentTest = extent.createTest(testName);
+        if(type.equals(ReportType.SINGLE)){
+            test.set(extentTest);
+            return;
+        }
+        parentTest.set(extentTest);
+    }
+
+    public static void createStep(String stepName){
+        try{
+            ExtentTest child = parentTest.get().createNode(stepName);
+            test.set(child);
+        }catch (NullPointerException ignored){
+            ignored.printStackTrace();
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
